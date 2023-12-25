c2d0c2961c34a965e541dbe689ff1e42f5fa5209
eliotora <eliot.cosyn@gmail.com>
2023-12-25 13:21:42 +0100

    Added some mazes Created a function to build initial config from string

---
+++ src/environments/utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+from envs.examples import *
+
+
+def grid_from_string(grid_str):
+    s = grid_str.split('\n')
+    grid = []
+    starting_pos = {}
+    for ri, r in enumerate(s):
+        row = []
+        for ci, c in enumerate(r):
+            if c == "." or 'A' <= c <= 'Z' or 'a' <= c <= 'z':
+                row.append([int(s[ri - 1][ci] != "_"), int(s[ri][ci + 1] != "|"), int(s[ri + 1][ci] != "_"),
+                            int(s[ri][ci - 1] != "|")])
+            if c == 'M':
+                starting_pos['minautor'] = np.array([ri // 2 - 1, ci // 2])
+            elif 'A' <= c <= 'Z' or 'a' <= c <= 'z':
+                starting_pos[str('player_' + c)] = np.array([ri // 2 - 1, ci // 2])
+            if c == '#':
+                if ri % 2 == 0 and ci % 2 == 0:
+                    if ci == 0:
+                        starting_pos['exit'] = np.array([ri // 2 - 1, ci - 1])
+                    else:
+                        starting_pos['exit'] = np.array([ri // 2 - 1, ci // 2])
+                elif ri % 2 == 1 and ci % 2 == 1:
+                    if ri == 1:
+                        starting_pos['exit'] = np.array([ri // 2 - 1, ci // 2])
+                    else:
+                        starting_pos['exit'] = np.array([ri // 2 + 1, ci // 2])
+        if row:
+            grid.append(row)
+    return grid, starting_pos
+
+
+if __name__ == '__main__':
+    grid, starting_pos = grid_from_string(simple_maze)
+    print(starting_pos)
+    grid, starting_pos = grid_from_string(maze_1)
+    print(starting_pos)
+    grid, starting_pos = grid_from_string(maze_10)
+    print(starting_pos)
+    grid, starting_pos = grid_from_string(maze_12)
+    print(starting_pos)
