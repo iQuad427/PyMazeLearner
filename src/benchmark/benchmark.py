c88a2b365515b5da878bd6bce7ce6c418ab6d263
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-12-28 15:52:59 +0100

    Cleaning up benchmark

--- src/benchmark/benchmark.py
+++ src/benchmark/benchmark.py
@@ -1,9 +1,12 @@
-import abc
-import math
 import os
 from collections import defaultdict
 from typing import Dict, Callable
 
+from src.benchmark.runnables import (
+    BaseRunnable,
+    RunnableQLearner,
+    RunnableProgressiveQLearner,
+)
 from src.environments.envs.examples import (
     maze_1,
     maze_2,
@@ -20,94 +23,8 @@ from src.environments.envs.examples import (
     maze_14,
     maze_15,
 )
-from src.learners.progressive_qlearner import ProgressiveQLearner
-from src.learners.qlearner import QLearner
-from src.learners.symbolic_learner.learner import SymbolicLearner
 from src.learners.symbolic_learner.models.c45 import C45Model
 from src.learners.symbolic_learner.models.naive_bayes import NaiveBayesModel
-from src.runner.runner import Runner
-
-
-class BaseRunnable(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def run(self, mazes, **kwargs):
-        raise NotImplementedError
-
-
-class RunnableQLearner(BaseRunnable):
-    def __init__(self):
-        self.events = {}
-
-    def run(self, mazes, **kwargs):
-        for maze in mazes:
-            try:
-
-                def log_first_win_and_stop(env, event):
-                    if event.name == "first_win":
-                        self.events[maze] = event
-                        env.force_stop = True
-
-                runner = Runner(
-                    maze=mazes[maze],
-                    agent_builder=lambda _, grid_shape: QLearner(grid_shape),
-                    event_callback=log_first_win_and_stop,
-                )
-
-                runner.run()
-            except Exception as e:
-                print(
-                    "An error occurred while running the QLearner on maze {0}.".format(
-                        maze
-                    )
-                )
-                print(e)
-        return self.events
-
-
-class RunnableProgressiveQLearner(BaseRunnable):
-    def __init__(self):
-        self.events = {}
-
-    def run(self, mazes=None, model_factory=None,cumulative=False, **kwargs):
-        symbolic_learners = defaultdict(lambda: SymbolicLearner(model_factory))
-
-        for index, (name, maze) in enumerate(mazes.items()):
-
-            def log_first_win_and_stop(env, event):
-                if event.name == "first_win":
-                    self.events[name] = event
-                    # Here we do not want to stop the environment, because we want to train the symbolic model.
-
-            runner = Runner(
-                maze=maze,
-                agent_builder=lambda agent, grid_shape: ProgressiveQLearner(
-                    grid_shape,
-                    # We only want to use the symbolic model once it has been trained.
-                    predict=symbolic_learners[agent].predict if index > 0 else None,
-                ),
-                event_callback=log_first_win_and_stop,
-            )
-
-            if not cumulative:
-                symbolic_learners.clear()
-
-            runner.run()
-
-            runner.configure(
-                train=False,
-                convergence_count=math.inf,
-                iterations=1_000,
-                action_logger=lambda agent, state, action: symbolic_learners[agent].log(
-                    state, action
-                ),
-            )
-
-            runner.run()
-
-            for agent in symbolic_learners:
-                symbolic_learners[agent].train()
-
-        return self.events
 
 
 def retrieve_number_of_steps_and_episodes_per_maze(
@@ -140,7 +57,9 @@ def benchmark_runnables(
 
     for _ in range(runs):
         for name, runnable_factory in runnables.items():
-            events = runnable_factory().run(mazes, **(runnables_kwargs or {}).get(name, {}))
+            events = runnable_factory().run(
+                mazes, **(runnables_kwargs or {}).get(name, {})
+            )
 
             for maze, event in events.items():
                 number_of_steps[name][maze].append(event.data["number_of_steps"])
@@ -159,8 +78,6 @@ def benchmark_runnables(
                     / len(number_of_episodes[model][maze]),
                 )
             )
-
-
 
 
 if __name__ == "__main__":
