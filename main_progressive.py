788107400b40c69955d34db798c8f9de556f0cf0
iQuad <quentin.roels@gmail.com>
2023-12-31 17:20:39 +0100

    changed max_steps

--- main_progressive.py
+++ main_progressive.py
@@ -7,8 +7,8 @@ from src.java_interop_utils import safe_init_jvm, safe_stop_jvm
 from src.learners.progressive_qlearner import ProgressiveQLearner
 from src.learners.symbolic_learner.learner import SymbolicLearner
 from src.learners.symbolic_learner.models.naive_bayes import NaiveBayesModel
-from src.learners.symbolic_learner.models.weka.naive_bayes import NaiveBayesWekaModel
-from src.learners.symbolic_learner.models.weka.part_dt import PDTWekaModel
+# from src.learners.symbolic_learner.models.weka.naive_bayes import NaiveBayesWekaModel
+# from src.learners.symbolic_learner.models.weka.part_dt import PDTWekaModel
 from src.runner.runner import Runner
 from src.environments.observer.generic import generic_default, GenericObserver
 
@@ -31,51 +31,44 @@ if __name__ == "__main__":
 
     runner = Runner(
         enable_observation=False,
-        maze=maze_3,
+        convergence_count=math.inf,
+        maze=maze_7,
         agent_builder=lambda _, grid_shape: ProgressiveQLearner(grid_shape),
         n_agents=1,
-        iterations=1_0000,
-        max_steps=1_000,
+        iterations=10_000,
+        max_steps=math.inf,
         observer=observer,
-        # render_mode="human",
-        # sleep_time=0.01
     )
     runner.run()
 
-
     symbolic_learners = defaultdict(lambda: SymbolicLearner(RandomForestModel))
-    #
+
     runner.configure(
         train=False,
         convergence_count=math.inf,
-        iterations=1_00,
+        iterations=1_000,
         enable_observation=True,
         action_logger=lambda agent, state, action: symbolic_learners[agent].log(
             state, action
         ),
     )
-    print("Running ProgressiveQLearner on maze 7.")
 
     runner.run()
 
-    # print(dict(symbolic_learners))
-    his = [symbolic_learners[agent].history for agent in symbolic_learners]
-    for key in his[0].keys():
-        print(key.flatten(), his[0][key])
-    print(len(his[0]))
-    # input("Press Enter to continue...")
     symbolic_learners = dict(symbolic_learners)
 
     # Train the symbolic models.
     for agent in symbolic_learners:
         symbolic_learners[agent].train()
-    #
+
     progressive_runner = Runner(
-        maze=maze_3,
+        maze=maze_6,
         agent_builder=lambda agent, grid_shape: ProgressiveQLearner(
             grid_shape, predict=symbolic_learners[agent].predict
         ),
-        render_mode="human",
+        convergence_count=300,
+        max_steps=math.inf,
+        iterations=1_000,
         observer=observer
     )
 
@@ -84,6 +77,7 @@ if __name__ == "__main__":
     progressive_runner.configure(
         train=False,
         convergence_count=math.inf,
+        iterations=1,
         render_mode="human",
         sleep_time=0.2,
     )
